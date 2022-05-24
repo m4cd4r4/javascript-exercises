54c6ff7261dc6682a29b3455780304ad0bde3d92
m4cd4r4 <m4cd4r4@gmail.com>
2022-05-24 11:46:39 +0800

    Complete 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, number) {
+    newString = "";
+    
+    if (typeof number ==='number' && number >=0) {
+        for(let i = 0; i < number; i++){
+            newString += string;
+        } 
+        return newString;
+    };
+    
+    return "ERROR";
+    
+    }
 
+    
 // Do not edit below this line
 module.exports = repeatString;
